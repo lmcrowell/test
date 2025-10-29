ff7ec28a2be0b1198be3401bb1ae32ceb01c2a67
Lindsey Crowell <lmcrowell13@outlook.com>
2025-10-29 08:12:41 -0500

    Made changes to the plotting file

--- plotting.py
+++ plotting.py
@@ -4,14 +4,19 @@ import matplotlib.pyplot as plt
 
 x_vals = []
 y_vals = []
+z_vals = []
 
 for x in range(0,5):
     y = x + 1
+    z = x**2
     x_vals.append(x)
     y_vals.append(y)
+    z_vals.append(z)
 
-plt.plot(x_vals, y_vals, marker = 'o')
+plt.figure(1)
+plt.plot(x_vals, y_vals, marker = 'o', color = 'b')
+plt.plot(x_vals, z_vals, linestyle = '-', color = 'r')
 plt.xlabel("X-Values")
-plt.ylabel("Y-Values")
+plt.legend(["Y-Values", "Z-Values"])
 plt.title("Test Plot")
 plt.show()
